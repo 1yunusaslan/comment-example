01182e530d27b6591e0a001e5b89eaaeaf419f4c
Yunus Emre ASLAN <unuvitec@gmail.com>
2024-05-03 20:34:46 +0300

    completed

--- root/assets/js/main.js
+++ root/assets/js/main.js
@@ -4,6 +4,16 @@ let bookmarkCount = 0;
 
 
 
+const data = {
+    comment:{
+        comment_img:[
+            {
+                src:'./assets/img/kedi.jpg'
+            }
+        ]
+    }
+}
+
 
 document.getElementById("likeButton").onclick = function () {  
 
@@ -29,5 +39,31 @@ document.getElementById("bookmarkButton").onclick = function () {
 
 
 document.getElementById("shareButton").onclick = function () {  
-    alert("Foto root")
+    alert(data.comment.comment_img[0].src);
 }
+
+
+const commentResult = document.getElementById("comment-result");
+
+document.getElementById("comment-button").onclick= function(){
+
+    const commentInputValue = document.getElementById("comment-input").value;
+    
+
+
+    const makeP = document.createElement("p");
+    const makeDiv= document.createElement("div");
+    makeDiv.className="comment-result-layout";
+    const img = document.createElement("img")
+    img.src="./assets/img/me.jpg";
+
+    makeP.textContent = commentInputValue;
+    makeDiv.appendChild(img);
+    makeDiv.appendChild(makeP);
+    commentResult.appendChild(makeDiv);
+
+}
+
+
+
+
